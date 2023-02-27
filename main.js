bdeb085bf17c6abfa28f14ab480cc0c73a3ba1f1
Igor Czyż <igorczyz13@gmail.com>
2023-02-27 20:38:44 +0100

    Bug #016

--- main.js
+++ main.js
@@ -276,3 +276,21 @@ const test = () => {
 
 setTimeout(test, 2000)
 
+// ----------------------------------- Bug #015
+
+const myDog = new Dog ()
+const myCat = new Cat ()
+
+function Dog() {
+    this.name = 'Pluto'
+}
+
+class Cat {
+    constructor() {
+        this.name = 'Garfield'
+    }
+}
+
+console.log(myDog.name);
+console.log(myCat.name);
+
